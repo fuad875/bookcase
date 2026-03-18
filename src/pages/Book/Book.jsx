34465ed1412a2aefdc9c1d59e238907622f80126
fuad875 <fuad1115279890@gmail.com>
2026-03-18 16:45:52 +0600

    display data

---
+++ src/pages/Book/Book.jsx
@@ -0,0 +1,40 @@
+import React, { use } from 'react';
+import { FaStarHalfAlt } from "react-icons/fa";
+
+const Book = ({ singleBook }) => {
+    // const data = use(bookPromise)
+    // console.log(data)
+
+    // console.log(singleBook);
+
+    const {bookName,author,image,rating,category,tags,yearOfPublishing,publisher} =singleBook
+    return (
+        <div className="card bg-base-100 w-96 shadow-sm border p-6 shadow">
+            <figure className='p-4 bg-gray-200 w-2/3 mx-auto'>
+                <img className='h-[166px] object-cover hover:scale-100 transition-all hover:-rotate-2 duration-100'
+                    src={image}
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body ">
+             
+            <div className='flex justify-center gap-10'>
+                 {
+                tags.map(tag=> <button className=''>{tag}</button>)
+                }
+            </div>
+                <h2 className="card-title hover:scale-100 transition-all hover:-rotate-1 duration-100">
+                    {bookName}
+                    <div className="badge badge-secondary">{yearOfPublishing}</div>
+                </h2>
+                <p>book by:{publisher}</p>
+                <div className='border-t-1 border-dashed'></div>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline bg-secondary">{category}</div>
+                    <div className="badge badge-outline bg-amber-200">{rating} <FaStarHalfAlt /></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
