a58804eae0c1d22814afd3fd832c73eaf70d51d5
fuad875 <fuad1115279890@gmail.com>
2026-03-19 17:49:38 +0600

    all component

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoredDB } from '../../utility/addToDB';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { ToastContainer, toast } from 'react-toastify';
+
+const MySwal = withReactContent(Swal)
 
 const BookDetails = () => {
-    const {id} = useParams(); //router id 
+    const { id } = useParams(); //router id 
     const bookId = parseInt(id); // convert string to number
 
     const data = useLoaderData();
-   const singleBook = data.find(book=>book.bookId === bookId)
-     const {bookName,image} =singleBook || {} ;
+    const singleBook = data.find(book => book.bookId === bookId)
+    const { bookName, image } = singleBook || {};
 
 
-     const handleMarkAsRead =(id)=>{
-            addToStoredDB(id);
-     }
+    const handleMarkAsRead = (id) => {
+
+        // MySwal.fire({
+        //     title: <p>Hello World</p>,
+        //     didOpen: () => {
+        //         // `MySwal` is a subclass of `Swal` with all the same instance & static methods
+        //         MySwal.showLoading()
+        //     },
+        // }).then(() => {
+        //     return MySwal.fire(<p>Shorthand works too</p>)
+        // })
+
+        toast("Wow so easy!");
+        addToStoredDB(id);
+    }
     return (
         <div className='w-2/3 mx-auto'>
-            <img className='w-48' src={image} alt=''/>
+            <img className='w-48' src={image} alt='' />
             <h5>{bookName}</h5>
-
-            <button onClick={()=>handleMarkAsRead(id)} className="btn btn-accent m-2">Mark as Read</button>
-             <button className="btn btn-info m-2">Add To WishList</button>
+              <ToastContainer />
+            <button onClick={() => handleMarkAsRead(id)} className="btn btn-accent m-2">Mark as Read</button>
+            <button className="btn btn-info m-2">Add To WishList</button>
         </div>
     );
 };
--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -3,22 +3,46 @@ import { useLoaderData } from 'react-router';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { getStoredBook } from '../../utility/addToDB';
+import Book from '../Book/Book';
 
 const ReadList = () => {
-     
-    const[readList,setReadList] = useState();
-     const data = useLoaderData();
+
+    const [readList, setReadList] = useState([]);
+    const [sort,setSort] =useState("");
+    const data = useLoaderData();
     //  console.log(data)
-      
-     useEffect(()=>{
+
+    useEffect(() => {
         const storedBookData = getStoredBook();
-        const ConvertedStoredBooks = storedBookData.map(id=>parseInt(id))
-         const myReadList = data.filter(book => ConvertedStoredBooks.includes(book.bookId));
-           setReadList(myReadList);
-     },[])
+        const ConvertedStoredBooks = storedBookData.map(id => parseInt(id));
+        const myReadList = data.filter(book => ConvertedStoredBooks.includes(book.bookId));
+        setReadList(myReadList);
+    }, [])
+
+     const handleSort =(type) =>{
+        setSort(type);
+        if(type === "pages"){
+              const SortedByPage =[...readList].sort((a,b)=>a.totalPages -b.totalPages);
+              setReadList(SortedByPage);
+        }
+
+        if(type === "ratings"){
+            const sortedByratings = [...readList].sort((a,b)=>a.rating - b.rating);
+            setReadList(sortedByratings);
+        }
+     }
+
 
     return (
         <div>
+
+            <details className="dropdown">
+                <summary className="btn m-1">sort by:{sort ? sort : ""}</summary>
+                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li><a onClick={()=>handleSort("pages")}>pages</a></li>
+                    <li><a onClick={()=>handleSort("ratings")}>ratings</a></li>
+                </ul>
+            </details>
             <Tabs>
                 <TabList>
                     <Tab>Read Book List</Tab>
@@ -27,6 +51,10 @@ const ReadList = () => {
 
                 <TabPanel>
                     <h2>Book I read{readList.length}</h2>
+
+                    {
+                        readList.map(b => <Book key={b.bookId} singleBook={b}></Book>)
+                    }
                 </TabPanel>
                 <TabPanel>
                     <h2>My wish List</h2>
